797ef1ce30b0d2bc8110e300b5b0b1726b3849cc
Kcnarf <fl.franck.lebeau@gmail.com>
2022-08-16 11:33:50 +0200

    [fix] typo, -try to- make CI work on Ubuntu

--- test/face-test.js
+++ test/face-test.js
@@ -1,19 +1,19 @@
-var tape = require("tape"),
-    utils = require('../build/utils');
-    vertex = require("../build/Vertex");
-    face = require("../build/face");
+var tape = require('tape'),
+  utils = require('../build/utils');
+vertex = require('../build/vertex');
+face = require('../build/face');
 
-tape("Face(...) should set the expected defaults", function(test) {
+tape('Face(...) should set the expected defaults', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      f = new face.Face(v0, v1, v2),
-      normal = f.normal,
-      he0 = f.edges[0],
-      he1 = f.edges[1],
-      he2 = f.edges[2];
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    f = new face.Face(v0, v1, v2),
+    normal = f.normal,
+    he0 = f.edges[0],
+    he1 = f.edges[1],
+    he2 = f.edges[2];
 
-  test.deepEqual(f.verts, [v0,v1,v2]);
+  test.deepEqual(f.verts, [v0, v1, v2]);
   test.equal(f.marked, false);
   test.equal(normal.x, -0.8571428571428571);
   test.equal(normal.y, -0.42857142857142855);
@@ -33,18 +33,18 @@ tape("Face(...) should set the expected defaults", function(test) {
   test.end();
 });
 
-tape("Face(...) should handle orient", function(test) {
+tape('Face(...) should handle orient', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      v3 = new vertex.Vertex(-0.8571428571428571, -0.42857142857142855, -0.2857142857142857),
-      f = new face.Face(v0, v1, v2, v3),
-      normal = f.normal,
-      he0 = f.edges[0],
-      he1 = f.edges[1],
-      he2 = f.edges[2];
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    v3 = new vertex.Vertex(-0.8571428571428571, -0.42857142857142855, -0.2857142857142857),
+    f = new face.Face(v0, v1, v2, v3),
+    normal = f.normal,
+    he0 = f.edges[0],
+    he1 = f.edges[1],
+    he2 = f.edges[2];
 
-  test.deepEqual(f.verts, [v0,v2,v1]);
+  test.deepEqual(f.verts, [v0, v2, v1]);
   test.equal(normal.x, 0.8571428571428571);
   test.equal(normal.y, 0.42857142857142855);
   test.equal(normal.z, 0.2857142857142857);
@@ -63,28 +63,28 @@ tape("Face(...) should handle orient", function(test) {
   test.end();
 });
 
-tape("Face.getDualPoint() should return the expected Point2D", function(test) {
+tape('Face.getDualPoint() should return the expected Point2D', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      f = new face.Face(v0, v1, v2),
-      dualPoint = f.getDualPoint();
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    f = new face.Face(v0, v1, v2),
+    dualPoint = f.getDualPoint();
 
   test.equal(dualPoint.x, -1.5);
   test.equal(dualPoint.y, -0.75);
   test.end();
 });
 
-tape("Face.createEdges(...) should compute the expected HEdges", function(test) {
+tape('Face.createEdges(...) should compute the expected HEdges', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      f = new face.Face(v0, v1, v2);
-      
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    f = new face.Face(v0, v1, v2);
+
   f.createEdges();
   var he0 = f.edges[0],
-      he1 = f.edges[1],
-      he2 = f.edges[2];
+    he1 = f.edges[1],
+    he2 = f.edges[2];
 
   test.equal(he0.orig, v0);
   test.equal(he0.dest, v1);
@@ -101,19 +101,19 @@ tape("Face.createEdges(...) should compute the expected HEdges", function(test) 
   test.end();
 });
 
-tape("Face.orient(...) should computes the expected HEdges", function(test) {
+tape('Face.orient(...) should computes the expected HEdges', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      v3 = new vertex.Vertex(-0.8571428571428571, -0.42857142857142855, -0.2857142857142857),
-      f = new face.Face(v0, v1, v2);
-  
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    v3 = new vertex.Vertex(-0.8571428571428571, -0.42857142857142855, -0.2857142857142857),
+    f = new face.Face(v0, v1, v2);
+
   f.orient(v3);
   var he0 = f.edges[0],
-      he1 = f.edges[1],
-      he2 = f.edges[2];
+    he1 = f.edges[1],
+    he2 = f.edges[2];
 
-  test.deepEqual(f.verts, [v0,v2,v1]);
+  test.deepEqual(f.verts, [v0, v2, v1]);
   test.equal(he0.orig, v0);
   test.equal(he0.dest, v2);
   test.equal(he0.prev, he2);
@@ -129,33 +129,33 @@ tape("Face.orient(...) should computes the expected HEdges", function(test) {
   test.end();
 });
 
-tape("Face.getEdge(...) should return expected HEdge", function(test) {
+tape('Face.getEdge(...) should return expected HEdge', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      v3 = new vertex.Vertex(1, 2, 3),
-      f = new face.Face(v0, v1, v2),
-      he0 = f.edges[0],
-      he1 = f.edges[1],
-      he2 = f.edges[2];
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    v3 = new vertex.Vertex(1, 2, 3),
+    f = new face.Face(v0, v1, v2),
+    he0 = f.edges[0],
+    he1 = f.edges[1],
+    he2 = f.edges[2];
 
-  test.equal(f.getEdge(v0,v1), he0);
-  test.equal(f.getEdge(v1,v0), he0);
-  test.equal(f.getEdge(v0,v3), null);
+  test.equal(f.getEdge(v0, v1), he0);
+  test.equal(f.getEdge(v1, v0), he0);
+  test.equal(f.getEdge(v0, v3), null);
   test.end();
 });
 
-tape("Face.link(...) should find and link twins", function(test) {
+tape('Face.link(...) should find and link twins', function (test) {
   var v00 = new vertex.Vertex(1, 0, 0),
-      v01 = new vertex.Vertex(0, 2, 0),
-      v02 = new vertex.Vertex(0, 0, 3),
-      f0 = new face.Face(v00, v01, v02),
-      h00 = f0.edges[0],
-      v10 = new vertex.Vertex(1, 0, 0),
-      v11 = new vertex.Vertex(0, 2, 0),
-      v12 = new vertex.Vertex(0, 0, 3),
-      f1 = new face.Face(v10, v11, v12),
-      h10 = f1.edges[0];
+    v01 = new vertex.Vertex(0, 2, 0),
+    v02 = new vertex.Vertex(0, 0, 3),
+    f0 = new face.Face(v00, v01, v02),
+    h00 = f0.edges[0],
+    v10 = new vertex.Vertex(1, 0, 0),
+    v11 = new vertex.Vertex(0, 2, 0),
+    v12 = new vertex.Vertex(0, 0, 3),
+    f1 = new face.Face(v10, v11, v12),
+    h10 = f1.edges[0];
 
   f0.link(f1, v10, v11);
   test.equal(h00.twin, h10);
@@ -163,15 +163,15 @@ tape("Face.link(...) should find and link twins", function(test) {
   test.end();
 });
 
-tape("Face.conflict(...) should detect conflict", function(test) {
+tape('Face.conflict(...) should detect conflict', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      v3 = new vertex.Vertex(2, 0, 0),
-      v4 = new vertex.Vertex(-2, 0, 0),
-      v5 = new vertex.Vertex(1-utils.epsilon, 0, 0),
-      v6 = new vertex.Vertex(1-2*utils.epsilon, 0, 0),
-      f = new face.Face(v0, v1, v2);
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    v3 = new vertex.Vertex(2, 0, 0),
+    v4 = new vertex.Vertex(-2, 0, 0),
+    v5 = new vertex.Vertex(1 - utils.epsilon, 0, 0),
+    v6 = new vertex.Vertex(1 - 2 * utils.epsilon, 0, 0),
+    f = new face.Face(v0, v1, v2);
 
   test.notOk(f.conflict(v0));
   test.notOk(f.conflict(v3));
--- test/hEdge-test.js
+++ test/hEdge-test.js
@@ -1,13 +1,13 @@
-var tape = require("tape"),
-    vertex = require("../build/Vertex");
-    hEdge = require("../build/hEdge");
+var tape = require('tape'),
+  vertex = require('../build/vertex');
+hEdge = require('../build/hEdge');
 
-tape("HEdge(...) should set the expected defaults", function(test) {
+tape('HEdge(...) should set the expected defaults', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      face = {verts: [v0, v1, v2]},
-      he = new hEdge.HEdge(v0, v1, face);
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    face = { verts: [v0, v1, v2] },
+    he = new hEdge.HEdge(v0, v1, face);
 
   test.equal(he.next, null);
   test.equal(he.prev, null);
@@ -18,17 +18,17 @@ tape("HEdge(...) should set the expected defaults", function(test) {
   test.end();
 });
 
-tape("HEdge.isHorizon(...) should detect if it is an horizon", function(test) {
+tape('HEdge.isHorizon(...) should detect if it is an horizon', function (test) {
   var v00 = new vertex.Vertex(1, 0, 0),
-      v01 = new vertex.Vertex(0, 2, 0),
-      v02 = new vertex.Vertex(0, 0, 3),
-      face0 = {verts: [v00, v01, v02], marked: true},
-      he0 = new hEdge.HEdge(v00, v01, face0);
-      v10 = new vertex.Vertex(1, 0, 0),
-      v11 = new vertex.Vertex(0, 2, 0),
-      v12 = new vertex.Vertex(0, 0, 3),
-      face1 = {verts: [v10, v11, v12], marked: false};
-      he1 = new hEdge.HEdge(v10, v11, face1);
+    v01 = new vertex.Vertex(0, 2, 0),
+    v02 = new vertex.Vertex(0, 0, 3),
+    face0 = { verts: [v00, v01, v02], marked: true },
+    he0 = new hEdge.HEdge(v00, v01, face0);
+  (v10 = new vertex.Vertex(1, 0, 0)),
+    (v11 = new vertex.Vertex(0, 2, 0)),
+    (v12 = new vertex.Vertex(0, 0, 3)),
+    (face1 = { verts: [v10, v11, v12], marked: false });
+  he1 = new hEdge.HEdge(v10, v11, face1);
 
   test.notOk(he0.isHorizon());
 
@@ -44,15 +44,15 @@ tape("HEdge.isHorizon(...) should detect if it is an horizon", function(test) {
   test.end();
 });
 
-tape("HEdge.isEqual(...) should detect if it corresponds to its orig and dest", function(test) {
+tape('HEdge.isEqual(...) should detect if it corresponds to its orig and dest', function (test) {
   var v0 = new vertex.Vertex(1, 0, 0),
-      v1 = new vertex.Vertex(0, 2, 0),
-      v2 = new vertex.Vertex(0, 0, 3),
-      face = {verts: [v0, v1, v2]},
-      he = new hEdge.HEdge(v0, v1, face),
-      w0 = new vertex.Vertex(1, 0, 0),
-      w1 = new vertex.Vertex(0, 2, 0),
-      w2 = new vertex.Vertex(1, 2, 3);
+    v1 = new vertex.Vertex(0, 2, 0),
+    v2 = new vertex.Vertex(0, 0, 3),
+    face = { verts: [v0, v1, v2] },
+    he = new hEdge.HEdge(v0, v1, face),
+    w0 = new vertex.Vertex(1, 0, 0),
+    w1 = new vertex.Vertex(0, 2, 0),
+    w2 = new vertex.Vertex(1, 2, 3);
 
   test.ok(he.isEqual(w0, w1));
   test.ok(he.isEqual(w1, w0));
@@ -60,4 +60,3 @@ tape("HEdge.isEqual(...) should detect if it corresponds to its orig and dest", 
   test.notOk(he.isEqual(w2, w0));
   test.end();
 });
-
